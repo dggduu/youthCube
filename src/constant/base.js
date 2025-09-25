11848dec91da9a71564f43b5c2823e5cdc40fc5e
dggduu <aobaradgg@proton.me>
2025-09-25 13:08:47 +0800

    down

--- src/constant/base.js
+++ src/constant/base.js
@@ -3,8 +3,8 @@ export const BASE_INFO={
         url: 'http://10.0.2.2:1234',
         model: 'qwen2-vl-2b-instruct',
     },
-    BASE_URL: 'http://aoicube.dpdns.org:3166/v1/',
-    BASE_SOCKET_URL: 'wss://aoicube.dpdns.org:3166',
+    BASE_URL: 'https://aoicube.dpdns.org/v1/',
+    BASE_SOCKET_URL: 'wss://aoicube.dpdns.org',
     fakeUser: {
         id: '8',
         email: 'dev@example.com',
@@ -19,6 +19,3 @@ export const BASE_INFO={
         isSkipRegisterVerify: false,
     },
 };
-
-    // BASE_URL: 'http://aoicube.dpdns.org:3166/v1/',
-    // BASE_SOCKET_URL: 'wss://aoicube.dpdns.org:3166',
